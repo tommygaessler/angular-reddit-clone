99779189a72b8a65f36dd438bc9084da31b6f8ba
Tommy Gaessler <tommy.gaessler@gmail.com>
2016-10-16 20:54:56 -0600

    made ui

--- controllers.js
+++ controllers.js
@@ -4,6 +4,33 @@
 
   const app = angular.module('reddit-clone');
 
-  
+  app.controller('redditController', function() {
+
+
+
+
+
+
+
+
+
+    this.posts = [
+      {
+        title: 'Apple',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/2000px-Apple_logo_black.svg.png',
+        votes: 0,
+        author: 'Steve Jobs',
+        description: 'Apple Inc. is an American multinational technology company headquartered in Cupertino, California, that designs, develops, and sells consumer electronics, computer software, and online services.',
+        date_posted: 'fake date',
+        total_comments: 1,
+        comments: [
+          {
+            author: 'Tommy',
+            comment: 'I love apple!'
+          }
+        ]
+      }
+    ];
+  });
 
 }());
